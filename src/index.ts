63c88f50d224d9e375ece34e5fbb1947e2576380
nyx <nyx.law.official@gmail.com>
2024-04-10 18:49:18 +0800

    feat(ts): 利用陣列記錄多筆產品

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { Product } from "./product"
-const product = new Product(
+const product = [ new Product(
     1,
     "圖像 Angular 開發入門 第二版",
     "博碩文化",
     500,
     true,
     new Date(),
-);
+),
+new Product(
+    1,
+    "金魚都能懂的 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(),
+),
+];
 console.log(product);
