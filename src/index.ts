301d084c7e4a3e620d0ab9aa18ad0f4afe98409e
nyx <nyx.law.official@gmail.com>
2024-04-10 18:18:31 +0800

    feat(ts): 利用類別定義產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-const product ={
-    id:1,
-    name: "圖像 Angular 開發入門 第二版",
-    company: "博碩文化",
-    price:500,
-    isShow: true,
-    createDate: new Date(),
-    modifyDate: undefined,
-};
+import { Product } from "./product"
+const product = new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(),
+    undefined
+);
 console.log(product);
 console.table(product);
