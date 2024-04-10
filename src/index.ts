354a9c4908adcbe5649df0884e65e295c090c251
nyx <nyx.law.official@gmail.com>
2024-04-10 20:01:59 +0800

    feat(ts): 利用類別方法設定產品是否顯示

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 import { Product } from "./product"
-const product = [ new Product(
-    1,
-    "圖像 Angular 開發入門 第二版",
-    "博碩文化",
-    500,
-    true,
-    new Date(),
-),
-new Product(
-    1,
-    "金魚都能懂的 CSS 必學屬性",
-    "博碩文化",
-    500,
-    true,
-    new Date(),
-),
+const product = [ 
+    new Product(
+        1,
+        "圖像 Angular 開發入門 第二版",
+        "博碩文化",
+        500,
+        true,
+        new Date(),
+    ),
+    new Product(
+        1,
+        "金魚都能懂的 CSS 必學屬性",
+        "博碩文化",
+        500,
+        true,
+        new Date(),
+    ),
 ];
-setProductDisplay(product[1], false);
+product[1].setDisplay(false);
 console.log(product);
-
-function setProductDisplay(product:Product, isShow:boolean){
-    product.isShow = isShow;
-}
