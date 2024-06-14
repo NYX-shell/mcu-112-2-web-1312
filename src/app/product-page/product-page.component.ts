f1539278e605f08171b7f859ec6cb824d336392e
nyx <nyx.law.official@gmail.com>
2024-06-15 02:51:02 +0800

    feat(product): 使用 AsynPipe 顯示產品清單資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -4,34 +4,26 @@ import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [ProductCardListComponent],
+  imports: [AsyncPipe, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css'
 })
-export class ProductPageComponent implements OnInit{
+export class ProductPageComponent {
   router = inject(Router);
 
   private productService = inject(ProductService);
 
-  products!: Product[];
-
   private readonly refresh$ = new Subject<void>();
 
-  ngOnInit(): void{
-    this.refresh$
-      .pipe(
-        startWith(undefined),
-        switchMap(()=>this.productService.getList())
-      )
-      .subscribe(
-        (products) => (this.products = products)
-      )
-    ;
-  }
+  readonly products$ = this.refresh$.pipe(
+    startWith(undefined),
+    switchMap(()=>this.productService.getList())
+  );
 
   onAdd():void{
     const product = new Product({
